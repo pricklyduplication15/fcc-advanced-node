714d87829dd92c6cc0422bc7f376c92928dd4220
Yon Wayne <chandler.taylor15@gmail.com>
2024-05-28 20:20:42 +0000

    penultimate

--- server.js
+++ server.js
@@ -83,13 +83,20 @@ async function run() {
     let currentUsers = 0;
     io.on("connection", (socket) => {
       ++currentUsers;
-      io.emit("user count", currentUsers);
+      io.emit("user", {
+        username: socket.request.user.username,
+        currentUsers,
+        connected: true,
+      });
       console.log("A user has connected");
-
       socket.on("disconnect", () => {
         console.log("A user has disconnected");
         --currentUsers;
-        io.emit("user count", currentUsers);
+        io.emit("user", {
+          username: socket.request.user.username,
+          currentUsers,
+          connected: false,
+        });
       });
     });
   } catch (e) {
