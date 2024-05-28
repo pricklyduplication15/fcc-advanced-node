1d1bcc9195cf801860c9e93e3751c223ed6b2785
Yon Wayne <chandler.taylor15@gmail.com>
2024-05-28 16:53:22 +0000

    feat: social oauth

--- routes.js
+++ routes.js
@@ -8,61 +8,73 @@ module.exports = function (app, myDataBase) {
       message: "Please log in",
       showLogin: true,
       showRegistration: true,
+      showSocialAuth: true,
     });
+  });
+
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      }
+    );
+
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+    res.render("profile", { username: req.user.username });
+  });
+
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
   });
 
   app.route("/register").post(
     (req, res, next) => {
       const hash = bcrypt.hashSync(req.body.password, 12);
       myDataBase.findOne({ username: req.body.username }, (err, user) => {
-        if (err) return next(err);
-        if (user) return res.redirect("/");
-
-        myDataBase.insertOne(
-          {
-            username: req.body.username,
-            password: hash,
-          },
-          (err, doc) => {
-            if (err) return res.redirect("/");
-            next(null, doc.ops[0]);
-          }
-        );
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: hash,
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
       });
     },
     passport.authenticate("local", { failureRedirect: "/" }),
-    (req, res) => {
+    (req, res, next) => {
       res.redirect("/profile");
     }
   );
 
-  app.post(
-    "/login",
-    passport.authenticate("local", {
-      failureRedirect: "/",
-      successRedirect: "/profile",
-    }),
-    (req, res) => {
-      console.log("User authenticated:", req.isAuthenticated());
-    }
-  );
+  app.route("/auth/github").get(passport.authenticate("github"));
+  app
+    .route("/auth/github/callback")
+    .get(
+      passport.authenticate("github", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      }
+    );
 
-  app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render("profile", {
-      username: req.user.username,
-    });
-  });
-
-  app.route("/logout").post((req, res) => {
-    try {
-      req.logout(() => {
-        req.session.destroy(() => {
-          res.redirect("/");
-        });
-      });
-    } catch (error) {
-      console.error(error);
-    }
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found");
   });
 };
 
